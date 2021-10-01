683d99f870a7f51b6a79070bc6362eccb886fa1e
PRAYOON SUNTORNKITI (CDGS-ADD) <prayoon.su@cdg.co.th>
2021-10-01 14:40:07 +0700

    add mock api

--- day02/src/main/java/com/example/day02/Day02Application.java
+++ day02/src/main/java/com/example/day02/Day02Application.java
@@ -2,12 +2,17 @@ package com.example.day02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Day02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day02Application.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context
+                = SpringApplication.run(Day02Application.class, args);
+        System.out.println(context.getBeanDefinitionCount());
+    }
 
 }
---
+++ mock-api/readme.md
@@ -0,0 +1,3 @@
+https://github.com/azagniotov/stubby4j#quick-start-example
+
+java -jar stubby4j-7.3.3.jar -d api.yml
