735897984b705689a2a620232307ae24b8f389d5
PRAYOON SUNTORNKITI (CDGS-ADD) <prayoon.su@cdg.co.th>
2021-10-01 14:48:06 +0700

    add spring boot test

--- day02/src/main/java/com/example/day02/Day02Application.java
+++ day02/src/main/java/com/example/day02/Day02Application.java
@@ -1,18 +1,20 @@
 package com.example.day02;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Arrays;
 
+@Slf4j
 @SpringBootApplication
 public class Day02Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context
                 = SpringApplication.run(Day02Application.class, args);
-        System.out.println(context.getBeanDefinitionCount());
+       log.debug("initial total beans : {}" , context.getBeanDefinitionCount());
     }
 
 }
---
+++ day02/src/test/java/com/example/day02/employee/EmployeeControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.day02.employee;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @Test
+    public void getById(){
+        Integer id = 1;
+        EmployeeResponse  response =  restTemplate.getForObject("/employee/" + id, EmployeeResponse.class);
+        log.debug(response.getName());
+        assertEquals(id,response.getId());
+
+    }
+}
